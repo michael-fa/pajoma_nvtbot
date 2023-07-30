9b41a201f90743f06740acc33d0256015ad044e2
michael-fa <michixx.bat@gmail.com>
2023-07-30 19:56:27 +0200

    Changed path symbols linux related

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using OpenQA.Selenium;
 using pajoma_nvtbot.Discord;
+using pajoma_nvtbot.Utils;
+using pajoma_nvtbot.Utils.IniFile;
 using pajoma_nvtbot.Web;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Xml.Linq;
@@ -12,12 +15,18 @@ namespace pajoma_nvtbot
     internal class Program
     {
         internal static bool g_run = true;
+        public static string m_NvtLink = null!;
 
+        public static Properties m_Basecfg = null!;
+
+
+
+#if !LINUX
         [DllImport("Kernel32")]
         static public extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
         public delegate bool EventHandler(CtrlType sig);
         public static EventHandler? m_Handler;
-        public static string m_NvtLink = null!;
+
 
         public enum CtrlType
         {
@@ -54,6 +63,23 @@ namespace pajoma_nvtbot
             g_run = false;
             return;
         }
+#endif
+
+#if LINUX
+        public static void StopSafely()
+        {
+            //SAVE GLOBAL PROPERTIES FILES
+            if (m_Basecfg != null) m_Basecfg.Save();
+
+            foreach (Users.User u in m_UserList)
+            {
+                u.StopBotThread();
+
+            }
+            g_run = false;
+            return;
+        }
+#endif
 
 
 
@@ -61,28 +87,29 @@ namespace pajoma_nvtbot
 
         static void Main(string[] args)
         {
-#if DEBUG
-             Console.WriteLine("------ DEBUGMODE!!!!");
-#endif
+    
 
+#if DEBUG
+            Console.WriteLine("------ DEBUGMODE!!!!");
+#endif          
+
+#if !LINUX
             Console.OutputEncoding = Encoding.Unicode;
             m_Handler += new EventHandler(Handler);
             SetConsoleCtrlHandler(m_Handler, true);
+#endif
 
-            if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\Users\\"))
-                Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\Users\\");
+            if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/Users/"))
+                Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + @"/Users/");
 
 
 
-            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\config.ini"))
-                File.Create(AppDomain.CurrentDomain.BaseDirectory + "\\config.ini");
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"/config.ini"))
+                File.Create(AppDomain.CurrentDomain.BaseDirectory + @"/config.ini");
 
-            IniFile basecfg = new IniFile(AppDomain.CurrentDomain.BaseDirectory + "\\config.ini");
-            if (!basecfg.KeyExists("auth-token", "Discord Bot")) basecfg.Write("auth-token", "changeme", "Discord Bot");
+            m_Basecfg = new Properties(AppDomain.CurrentDomain.BaseDirectory + @"/config.ini");
 
-            if (!basecfg.KeyExists("url", "Novatime WEB Driver")) basecfg.Write("url", "changeme", "Novatime WEB Driver");
-
-            if (basecfg.Read("auth-token", "Discord Bot").Equals("changeme"))
+            if (m_Basecfg.get("auth-token", "changeme").Equals("changeme"))
             {
                 Console.WriteLine("Discord Bot Token not set! Please change it to a valid bot token in settings.cfg");
                 Console.ReadLine();
@@ -90,27 +117,22 @@ namespace pajoma_nvtbot
                 return;
             }
 
-            if (basecfg.Read("url", "Novatime WEB Driver").Equals("changeme"))
+            if (m_Basecfg.get("url", "changeme").Equals("changeme"))
             {
                 Console.WriteLine("Novatime WEB Url not set! Please change it to a valid bot token in settings.cfg");
                 Console.ReadLine();
                 Program.StopSafely();
                 return;
             }
-            else m_NvtLink = basecfg.Read("url", "Novatime WEB Driver");
 
-
-
-
-
-            MainBot.Init(basecfg.Read("auth-token", "Discord Bot"));
+            MainBot.Init(m_Basecfg.get("auth-token"));
             MainBot.Start();
 
-            foreach (string file in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\Users\\"))
+            foreach (string file in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"/Users/"))
             {
                 Console.WriteLine("Found user " + file);
                 string fl = file.Replace(".ini", "").PadRight(1);
-                string[] xfl = fl.Split('\\');
+                string[] xfl = fl.Split('/');
 
 
                 m_UserList.Add(new Users.User(int.Parse(xfl[xfl.Length - 1])));
