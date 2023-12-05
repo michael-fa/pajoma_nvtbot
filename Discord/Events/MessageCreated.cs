c600e6935147c9c2fa9de1127fee12d4706a667e
michael-fa <michixx.bat@gmail.com>
2023-12-05 16:45:16 +0100

    Fehlernachricht beim anmelden im discord umgeschrieben

--- Discord/Events/MessageCreated.cs
+++ Discord/Events/MessageCreated.cs
@@ -22,17 +22,23 @@ namespace pajoma_nvtbot.Discord
                 {
                     case "register":
                         //arg.Message.Channel.SendMessageAsync("Hello");
-                        if(cmdSplit.Length != 3) { arg.Message.Channel.SendMessageAsync("Nutze /register (Nummer) (Password) oder /stop (Nummer) | Urlaub: W.I.P");  break; }
+                        if(cmdSplit.Length != 3) { arg.Message.Channel.SendMessageAsync("Nutze /register (Nummer) (Password) oder /stop (Nummer) | Urlaub: W.I.P"); break; }
 
                         int plchd;
                         if (int.TryParse(cmdSplit[1], out plchd) || !string.IsNullOrEmpty(cmdSplit[2]) || !string.IsNullOrWhiteSpace(cmdSplit[2]))
                         {
+                            if(Utils.UserTools.UserExists(plchd))
+                            {
+                                arg.Message.Channel.SendMessageAsync("Die BenutzerID " + plchd + " wurde bereits registrieret.");
+                                break;
+                            }
                             Program.m_UserList.Add(new Users.User(Convert.ToInt16(cmdSplit[1]), cmdSplit[2], arg.Channel.Id.ToString()));
                             arg.Message.Channel.SendMessageAsync("Infonachrichten Timer für tägliche Leidensstatistik aktiviert.");
                         }
                         else
                         {
-                            arg.Message.Channel.SendMessageAsync("Ein Personalzeitkonto unter den angegeben Daten existiert nicht oder das Passwort war falsch.");
+                            arg.Message.Channel.SendMessageAsync("Bitte prüfe deine angegeben Daten, es scheint als wären Sie nicht in Ordnung gewesen.");
+                            arg.Message.Channel.SendMessageAsync("Nutze /register (Nummer) (Password) oder /stop (Nummer) | Urlaub: W.I.P");
                         }
                         break;
 
